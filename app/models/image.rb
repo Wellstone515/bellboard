9fed83dd4954f28cf344d3db3033f2ef355d9b3d
Wellstone515 <info.fullmoons@gmail.com>
2019-07-15 13:16:29 +0900

    Update model

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,5 @@
 class Image < ApplicationRecord
+  belongs_to :post
+
+  mount_uploader :image_id, ImageUploader
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,9 @@
 class Post < ApplicationRecord
   belongs_to :user
   has_many :comments
+  has_many :images
+  has_many :favorites
+  has_many :favorited_users, through: :favorites, source: :user
+
+  accepts_nested_attributes_for :images
 end
